75442bf1296871dee6277e19a7c54cb0d07d1083
sandesan88 <sander.s.andresen@gmail.com>
2024-02-16 10:24:21 +0100

    This is my code so far

---
+++ 1.1.r
@@ -0,0 +1,46 @@
+# install.packages("readxl")
+
+library(readxl)
+
+file_path <- '/Users/sanderandresen/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/Master DTU/2. Semester/TSA/Assignment 1/As1Test.xlsx'
+data <- read_excel(file_path)
+
+yt <- as.numeric(data[1, -1])
+xt <- seq(from = 2018, by = 1/12, length.out = length(yt))
+n = length(yt)
+print(xt)
+print(yt)
+
+# 1.1
+
+plot(xt, yt, type = 'b', 
+     main = "Driven cars against month since start of january",
+     xlab = "Month since January 2018",
+     ylab = "Motor driven vehicles"
+     )
+
+
+# 2.1
+p = 2
+X_sys <- cbind(1, xt)  # X matrix of system
+print(X_sys)
+Theta <- solve(t(X_sys) %*% (X_sys)) %*% t(X_sys) %*% yt
+print(Theta)
+
+
+y_hat <- X_sys %*% Theta # The values of the function
+
+# Calculating the variance
+e_ols <- yt - y_hat   # Errors
+#print(e_ols)
+RSS_ols <- t(e_ols)%*%e_ols  # Squared errors
+#print(RSS_ols)
+sigma2_ols <- as.numeric(RSS_ols/(n-p))  # variance, sigma^2
+#print(sigma2_ols)  
+V <- sigma2_ols * solve(t(X_sys)%*%X_sys)  # Variance of parameters
+print(V)
+
+b0 <- (sqrt(diag(V)))[1]
+b1 <- (sqrt(diag(V)))[2]
+print(b0)
+print(b1)
