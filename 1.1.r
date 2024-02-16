0e2dfd228a0cdf9718d580ccc843cfe4c265d8c9
sandesan88 <sander.s.andresen@gmail.com>
2024-02-16 10:55:22 +0100

    With corrected directory

--- 1.1.r
+++ 1.1.r
@@ -2,7 +2,7 @@
 
 library(readxl)
 
-file_path <- '/Users/sanderandresen/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/Master DTU/2. Semester/TSA/Assignment 1/As1Test.xlsx'
+file_path <- "As1Test.xlsx"
 data <- read_excel(file_path)
 
 yt <- as.numeric(data[1, -1])
