72b87017a40709b31823ddae1aa24d426f151e61
Chang-Rong Xue <changrong.xue@hotmail.com>
2024-02-23 14:26:38 +0100

    Final Update

--- main.R
+++ main.R
@@ -1,4 +1,7 @@
 library(readxl)
+library(tidyr)
+library(ggplot2)
+library(data.table)
 
 file_path <- "train.xlsx"
 data <- read_excel(file_path)
@@ -24,16 +27,7 @@ plot(xt, yt, type = 'b',
 # LS Estimator for OLS
 p = 2
 X <- cbind(1, xt)  # X matrix of system
-# X^T @ X is full rank / linear independent
-# print(X_sys)
-# print(t(X_sys))
-# print(t(X_sys) %*% X_sys)
-# rank <- qr(t(X_sys) %*% X_sys)$rank
-# print(rank)
-# print(ncol(X_sys))
-
 theta_hat <- solve(t(X) %*% X) %*% t(X) %*% yt
-print(theta_hat)
 
 # 2.2
 y_hat <- X %*% theta_hat
@@ -44,7 +38,6 @@ sigma2 <- as.numeric(RSS / (n - p))
 print(sigma2) 
 V <- sigma2 * solve(t(X) %*% X)
 print(V)
-
 
 # 2.3 
 x_new <- seq(from = 2023, by = 1/12, length.out = 12)
@@ -198,6 +191,7 @@ twlvstep_lamb_09  <- NA
 data <- data.frame(x = x, y = yt)
 lambda <- 0.9
 N <- length(yt)
+naive <- NA
 for (i in 11:N) {
   F_N <- F_N + lambda^(i - 1) * f(-(i - 1)) %*% t(f(-(i - 1)))
   h_N <- lambda * Linv %*% h_N + f(0) * yt[i]
@@ -207,19 +201,37 @@ for (i in 11:N) {
   onestep_lamb_09[i + 1] <- t(f(1)) %*% theta_N
   sixstep_lamb_09[i + 6] <- t(f(6)) %*% theta_N
   twlvstep_lamb_09[i + 12] <- t(f(12)) %*% theta_N
+  naive[i+1] <- yt[i]
 }
 
 data_onestep <- data.frame(x = seq(-(n - 1), 1, 1), y = onestep_lamb_09)
 data_sixstep <- data.frame(x = seq(-(n - 1), 6, 1), y = sixstep_lamb_09)
 data_twlvstep <- data.frame(x = seq(-(n - 1), 12, 1), y = twlvstep_lamb_09)
 
+colors = c("One-month step" = "blue",
+           "Six-month step" = "green", 
+           "Twelve-month step" = "red")
 plot_N <- ggplot(data, aes(x = x, y = yt)) +
   geom_point() + 
-  geom_point(data = data_onestep, aes(y = onestep_lamb_09), col = "blue", size = 3) +
-  geom_point(data = data_sixstep, aes(y = sixstep_lamb_09), col = "green", size = 3) +
-  geom_point(data = data_twlvstep, aes(y = twlvstep_lamb_09), col = "red", size = 3)
-
+  geom_point(data = data_onestep, aes(y = onestep_lamb_09, col = "One-month step"), size = 3) +
+  geom_point(data = data_sixstep, aes(y = sixstep_lamb_09, col = "Six-month step"), size = 3) +
+  geom_point(data = data_twlvstep, aes(y = twlvstep_lamb_09, col = "Twelve-month step"), size = 3) +
+  scale_color_manual(values = colors) +
+  xlab("Date") + ylab("Number of vehicles in total") + 
+  labs(title = "One-month, six-month and twelve-step ahead predictions")
 print(plot_N)
+
+data_naive <- data.frame(x=seq(-(n-1),1,1), y=naive)
+colors = c("Naive" = "green", "One-month step"="blue")
+plot_N <- ggplot(data, aes(x = x, y = yt)) +
+  geom_point() +
+  geom_point(data = data_naive, aes(y=naive, col= "Naive"), size=3)  +
+  geom_point(data = data_onestep, aes(y = onestep_lamb_09, col = "One-month step"), size = 3) +
+  scale_color_manual(values = colors) +
+  xlab("Date") + ylab("Number of vehicles in total") + 
+  labs(title = "Comparision between naive persistence and one-step prediction")
+print(plot_N)
+
 
 # 4.6 & 4.7 & 4.8 & 4.9 
 
@@ -271,6 +283,60 @@ ggplot(long_rmsd_data, aes(x = lambda, y = RMSD, color = Horizon, group = Horizo
   scale_color_manual(values = c("OneStep" = "red", "SixStep" = "blue", "TwelveStep" = "green")) +
   theme(legend.title = element_blank())  # Hides the legend title if desired
 
-# 4.10
+optimal_lmbd_onestep = 0.55 + (which.min(rsmd_onestep) - 1) * 0.01
+optimal_lmbd_sixstep = 0.55 + (which.min(rsmd_sixstep) - 1) * 0.01
+optimal_lmbd_twlvstep = 0.55 + (which.min(rsmd_twlvstep) - 1) * 0.01
 
+# 4.12
+iterate_pred <- function(lambda) {
+  N <- length(yt)
+  i <- 1
+  F_N <- (lambda^0) * f(0) %*% t(f(0))
+  h_N <- (lambda^0) * f(0) * yt[i]
+  theta_N <- solve(F_N) %*% h_N
+  onestep <- NA
+  sixstep  <- NA
+  twlvstep  <- NA
+  # iteration
+  for (i in 2:N) {
+    F_N <- F_N + lambda^(i - 1) * f(-(i - 1)) %*% t(f(-(i - 1)))
+    h_N <- lambda * Linv %*% h_N + f(0) * yt[i]
+    theta_N <- solve(F_N) %*% h_N
+  }
+  print(theta_N)
+  onestep <- t(f(1)) %*% theta_N
+  sixstep <- t(f(6)) %*% theta_N
+  twlvstep <- t(f(12)) %*% theta_N
+  return(list(onestep, sixstep, twlvstep)) 
+}
+
+# read test data 
+data_test <- read_excel("test.xlsx")
+ytest <- as.numeric(data_test[1, -1])
+xtest <- seq(from = 2023 - 1/12, by = 1/12, length.out = length(ytest))
+testdata <- data.frame(x = xtest, y = ytest)
+data <- data.frame(x - xt, y = yt)
+
+# make predictions 
+xpts <- c(2023 - 1/12, 2023 + 1/12 * 5, 2023 + 1/12 * 11)
+pred_optimal_lambda_for_onestep <- iterate_pred(optimal_lmbd_onestep)
+pred1 = data.frame(x = xpts, y = unlist(pred_optimal_lambda_for_onestep))
+pred_optimal_lambda_for_sixstep <- iterate_pred(optimal_lmbd_sixstep)
+pred2 = data.frame(x = xpts, y = unlist(pred_optimal_lambda_for_sixstep))
+pred_optimal_lambda_for_twlvstep <- iterate_pred(optimal_lmbd_twlvstep)
+pred3 = data.frame(x = xpts, y = unlist(pred_optimal_lambda_for_twlvstep))
+
+colors <-  c("Lambda = 0.55" = "green", 
+             "Lambda = 0.79" = "purple", 
+             "Lambda = 0.95" = "blue",
+             "Test Data" = "red")
+plot_N <- ggplot(data, aes(x = xt, y = yt)) +
+  geom_point() +
+  geom_point(data = testdata, aes(x = xtest, y = ytest, col = "Test Data")) +
+  geom_point(data = pred1, aes(x = x, y = y, col = "Lambda = 0.55")) +
+  geom_point(data = pred2, aes(x = x, y = y, col = "Lambda = 0.79")) +
+  geom_point(data = pred3, aes(x = x, y = y, col = "Lambda = 0.95")) + 
+  scale_color_manual(values = colors) +
+  xlab("Date since 2018") + ylab("Number of Vehicles") 
+print(plot_N)
 
